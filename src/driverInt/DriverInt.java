d5a1a1ecc5e6b02c2a4b7a36c4972fe065b17317
DESKTOP-4CUQ89R\JUBER AHMED <mohammed.juber1993@gmail.com>
2018-04-23 23:43:05 -0400

    Practice

--- src/driverInt/DriverInt.java
+++ src/driverInt/DriverInt.java
@@ -1,10 +1,12 @@
 package driverInt;
+import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 
 
 public class DriverInt{
+	public static Logger APPLICATION_LOGS =Logger.getLogger("devpinoyLogger");
 	protected WebDriver driver;
 
 	public void typeByXpath(String XpathLocators, String Values)	{
--- src/mainPackage/mainClass.java
+++ src/mainPackage/mainClass.java
@@ -6,16 +6,27 @@ import assignDriver.AssignDriver;
 import values.Values;
 import xPathLocators.XpathLocators;
 
+@Test
+
 public class mainClass extends AssignDriver{
+
+	//APPLICATION_LOGS.debug("My TEST Started");
+
+
 
 	XpathLocators  loc= new XpathLocators ();
 	Values val= new Values();
 
-	@Test
+	//@Test
 	public void testingFramework(){
+
+		APPLICATION_LOGS.debug("My Test Started");
 		typeByXpath(loc.emailxpath, val.Email);
 		typeByXpath(loc.idxpath, val.Pass );
+
+		APPLICATION_LOGS.debug("My TEST Ended");
 	}
+
 
 
 }
